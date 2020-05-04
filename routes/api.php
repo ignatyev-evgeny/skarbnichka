8bbb8db5074fd08ec9bcfdac0cd1c59e30c38c57
IGNATYEV <ceo@ignatyev.pro>
2020-05-05 02:09:06 +0300

    v0.13

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::get('objects', function () {
+    $objects = \App\Models\Objects::all();
+    $objects = $objects->toJson();
+    return response($objects, 200)->header('Content-Type', 'application/json');
+});
