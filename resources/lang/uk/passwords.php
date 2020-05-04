dea6a20e4df0c15d3c008cb5ed3bd2b31fdd9b23
IGNATYEV <ceo@ignatyev.pro>
2020-05-04 20:35:23 +0300

    v0.08

--- resources/lang/uk/passwords.php
+++ resources/lang/uk/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Ваш пароль скинуто!',
+    'sent' => 'Ми надіслали електронною поштою посилання для скидання пароля!',
+    'throttled' => 'Зачекайте, перш ніж повторити спробу.',
+    'token' => 'Цей маркер скидання пароля недійсний.',
+    'user' => "Ми не можемо знайти користувача з цією електронною адресою.",
 
 ];
